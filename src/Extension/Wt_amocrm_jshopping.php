49101df03c20aa4289756fe85dced72d4d6768a1
Sergey Tolkachyov <info@web-tolk.ru>
2023-03-09 09:04:56 +0400

    Add files via upload

---
+++ src/Extension/Wt_amocrm_jshopping.php
@@ -0,0 +1,592 @@
+<?php
+/**
+ * @package     WT Amo CRM JoomShopping
+ * @version     1.0.0
+ * @Author      Sergey Tolkachyov, https://web-tolk.ru
+ * @copyright   Copyright (C) 2022 Sergey Tolkachyov
+ * @license     GNU/GPL http://www.gnu.org/licenses/gpl-2.0.html
+ * @since       1.0.0
+ */
+
+// No direct access
+namespace Joomla\Plugin\System\Wt_amocrm_jshopping\Extension;
+defined('_JEXEC') or die;
+
+use Joomla\Application\SessionAwareWebApplicationInterface;
+use Joomla\CMS\Log\Log;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Webtolk\Amocrm\Amocrm;
+
+class Wt_amocrm_jshopping extends CMSPlugin
+{
+	protected $autoloadlanguage = true;
+
+	/** Header for debug element
+	 * @var String $debug_section_header
+	 * @since 1.1.0
+	 */
+	private $debug_section_header;
+
+	/** Debug data
+	 * @var String $debug_data
+	 * @since 1.0.0
+	 */
+	private $debug_data;
+
+	/** Debug output
+	 * @var String $debug_output
+	 * @since 1.0.0
+	 */
+	private $debug_output;
+
+	/**
+	 * Class Constructor
+	 * @param object $subject
+	 * @param array $config
+	 */
+	public function __construct( & $subject, $config )
+	{
+		parent::__construct( $subject, $config );
+		$this->loadLanguage();
+	}
+
+
+	/**
+	 * @param   string  $debug_section_header
+	 * @param   mixed   $debug_data
+	 *
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	private function prepareDebugInfo(string $debug_section_header, $debug_data): void
+	{
+
+		// Берем сессию только в HTML фронте
+		$session = (Factory::getApplication() instanceof SessionAwareWebApplicationInterface ? Factory::getApplication()->getSession() : null);
+		if (is_array($debug_data) || is_object($debug_data))
+		{
+			$debug_data = print_r($debug_data, true);
+		}
+		$debug_output = $session->get("amocrmjshoppingdebugoutput");
+
+		$debug_output .= "<details style='border:1px solid #0FA2E6; margin-bottom:5px;'>";
+		$debug_output .= "<summary style='background-color:#384148; color:#fff;'>" . $debug_section_header . "</summary>";
+		$debug_output .= "<pre style='background-color: #eee; padding:10px;'>";
+		$debug_output .= $debug_data;
+		$debug_output .= "</pre>";
+		$debug_output .= "</details>";
+
+		$session->set("amocrmjshoppingdebugoutput", $debug_output);
+
+	}// END prepareDebugInfo
+
+	/**
+	 * Create an order which might be 'created' or 'not created'. 'Created' orders are displaying in JoomShopping admin panel.
+	 * 'Not created' orders are hidden by filter.
+	 *
+	 * @param $order object JoomShopping order object
+	 * @param $cart  object JoomShopping cart object
+	 *
+	 * @since 1.0.0
+	 */
+	public function onAfterCreateOrderFull(&$order, &$cart): void
+	{
+
+		if ($this->params->get('order_trigger_event', 'always') == 'always')
+		{
+			$this->sendOrderToAmocrm($order->order_id);
+		}
+
+	}// end onAfterCreateOrderFull()
+
+	/**
+	 * Функция выполняет основную работу по добавлению заказа в Битрикс 24.
+	 *
+	 * @param $orderId
+	 *
+	 *
+	 * @since 1.0.0
+	 */
+	public function sendOrderToAmocrm($orderId)
+	{
+
+		if (empty($orderId))
+		{
+			return false;
+		}
+		if (!class_exists('JSFactory'))
+		{
+			require_once(JPATH_SITE . '/components/com_jshopping/bootstrap.php');
+		}
+
+		// Берем сессию только в HTML фронте
+		$session     = (Factory::getApplication() instanceof SessionAwareWebApplicationInterface ? Factory::getApplication()->getSession() : null);
+
+		$order       = \JSFactory::getTable('order', 'jshop');
+		$order->load($orderId);
+		$orderItems = $order->getAllItems();
+
+		$lead_data = [
+			'created_by'  => 0, //ID пользователя, создающий сделку. При передаче значения 0, сделка будет считаться созданной роботом. Поле не является обязательным
+			'name'        => (!empty($this->params->get('order_name_prefix')) ? $this->params->get('order_name_prefix') . ' ' . $order->order_number : $order->order_number),
+			'pipeline_id' => $this->params->get('pipeline_id'),
+			'price'       => (int) $order->order_total,
+		];
+
+		$f_name      = (!empty($order->d_f_name) ? $order->d_f_name : $order->f_name);//Имя
+		$l_name      = (!empty($order->d_l_name) ? $order->d_l_name : $order->l_name);//Фамилия
+		$m_name      = (!empty($order->d_m_name) ? $order->d_m_name : $order->m_name);//Отчество
+		$firma_name  = (!empty($order->d_firma_name) ? $order->d_firma_name : $order->firma_name);//Название фирмы
+		$client_type = $order->client_type;//Тип клиента: юрик или физик
+		$firma_code  = $order->firma_code;//Фирма: ИНН?
+		$street      = (!empty($order->d_street) ? $order->d_street : $order->street);//Улица
+		$street_nr   = (!empty($order->d_street_nr) ? $order->d_street_nr : $order->street_nr);//номер дома
+		$home        = (!empty($order->d_home) ? $order->d_home : $order->home);//дом
+		$apartment   = (!empty($order->d_apartment) ? $order->d_apartment : $order->apartment);//квартира/офис
+		$zip         = (!empty($order->d_zip) ? $order->d_zip : $order->zip);//индекс получателя
+		$city        = (!empty($order->d_city) ? $order->d_city : $order->city);//город
+		$state       = (!empty($order->d_state) ? $order->d_state : $order->state);//область/регион
+		$phone       = (!empty($order->d_phone) ? $order->d_phone : $order->phone);//телефон
+		$mobil_phone = (!empty($order->d_mobil_phone) ? $order->d_mobil_phone : $order->mobil_phone);//телефон мобильный
+		$contact     = [
+			'name' => 'Joomshopping customer from order #' . $order->order_number
+		];
+
+
+		if (isset($f_name) && !empty($f_name))
+		{
+			$contact['name']       = $f_name;
+			$contact['first_name'] = $f_name;
+
+		}
+		/**
+		 * @todo А если окажется, что имя из основных данных, а фамилия - из данных о доставке?
+		 */
+		if (isset($l_name) && !empty($l_name))
+		{
+			$contact['name']      = (isset($f_name) && !empty($f_name)) ? $order->f_name . ' ' . $l_name : $l_name;
+			$contact['last_name'] = $l_name;
+
+		}
+		/**
+		 * API Amo CRM очень привередливо. Не приемлет пустых значений или неправильные типы.
+		 * Поэтому проверяем на всё, что возможно перед отправкой.
+		 */
+		if ((isset($phone) && !empty($phone))
+			|| (isset($mobil_phone) && !empty($mobil_phone))
+		)
+		{
+			/**
+			 * Телефоны контакта
+			 */
+			$phones = [
+				'field_code' => 'PHONE',
+				'values'     => []
+			];
+
+			if (isset($phone) && !empty($phone))
+			{
+				$phones['values'][] = [
+					'enum_code' => 'WORK',
+					'value'     => $phone
+				];
+			}
+
+			if (isset($mobil_phone) && !empty($mobil_phone))
+			{
+				$phones['values'][] = [
+					'enum_code' => 'WORK',
+					'value'     => $mobil_phone
+				];
+			}
+			$contact['custom_fields_values'][] = $phones;
+		}
+
+		if ((isset($order->email) && !empty($order->email))
+			|| (isset($order->d_email) && !empty($order->d_email))
+		)
+		{
+			/**
+			 * E-mails контакта
+			 */
+			$emails = [
+				'field_code' => 'EMAIL',
+				'values'     => []
+			];
+
+			if (isset($order->email) && !empty($order->email))
+			{
+				$emails['values'][] = [
+					'enum_code' => 'WORK',
+					'value'     => $order->email
+				];
+			}
+
+			if (isset($order->d_email) && !empty($order->d_email) && $order->d_email !== $order->email)
+			{
+				$emails['values'][] = [
+					'enum_code' => 'WORK',
+					'value'     => $order->d_email
+				];
+			}
+			$contact['custom_fields_values'][] = $emails;
+		}
+
+		$this->prepareDebugInfo('custom fields', $this->params->get('fields'));
+		/**
+		 * Кастомные поля из настроек сопоставления
+		 */
+		$amo_to_jshopping_fields = (array) $this->params->get('fields');
+
+		if (is_countable($amo_to_jshopping_fields) && count($amo_to_jshopping_fields) > 0)
+		{
+			foreach ($amo_to_jshopping_fields as $key => $value)
+			{
+				$jshopping_field_value = '';
+				foreach ($value->storefield as $jshopping_field)
+				{
+
+					if (empty($order->$jshopping_field))
+					{
+						continue;
+					}
+
+					if ($value == "country")
+					{//Получаем название страны
+						$jshopping_field_value .= (string) $this->getCountryName($order->$value) . " ";
+					}
+					elseif ($value == "coupon_id")
+					{// Получаем код купона
+						$jshopping_field_value .= (string) $this->getCouponCode($order->$value) . " ";
+					}
+					elseif ($value == "shipping_method_id")
+					{//название способа доставки
+						$jshopping_field_value .= (string) $order->getShippingName() . " ";
+					}
+					elseif ($value == "payment_method_id")
+					{//название способа оплаты
+						$jshopping_field_value .= (string) $order->getPaymentName() . " ";
+					}
+					elseif ($value == "order_status")
+					{//название статуса заказа
+						$jshopping_field_value .= (string) $order->getStatus() . " ";
+					}
+					elseif ($value == "birthday" and ($order->$value == "0000-00-00" || $order->$value == ""))
+					{
+						continue;
+					}
+					elseif ($value == 'wt_sm_otpravka_pochta_ru_barcode')
+					{// трек-номер Почты России - WT SM Otpravka.pochta.ru
+						$jshopping_field_value .= (string) $session->get('wt_sm_otpravka_pochta_ru_barcode');
+					}
+					else
+					{
+						$jshopping_field_value .= (string) $order->$jshopping_field . ' ';
+					}
+
+				}
+
+				if (empty($jshopping_field_value))
+				{
+					continue;
+				}
+
+				$lead_custom_field_array             = [
+					'field_id' => (int) $value->amocrm_field_id,
+					'values'   => [
+						[
+							'value' => $jshopping_field_value
+						]
+					]
+				];
+				$lead_data["custom_fields_values"][] = $lead_custom_field_array;
+			}
+		}
+
+		$lead_data['_embedded']['contacts'][] = $contact;
+
+
+		if ($this->params->get('lead_tag_id', 0) > 0)
+		{
+			$lead_data['_embedded']['tags'][0]['id'] = (int) $this->params->get('lead_tag_id');
+		}
+
+		/**
+		 * Add UTMs into array
+		 */
+
+		$lead_data = $this->checkUtms($lead_data);
+		$leads[]   = $lead_data;
+		$this->prepareDebugInfo('Amo CRM lead data before sending', $lead_data);
+		/**
+		 * Create a lead
+		 */
+		$amocrm = new Amocrm();
+		$result = $amocrm->createLeadsComplex($leads);
+		$result = (array) $result;
+
+		$this->prepareDebugInfo('Amo CRM result info', $result);
+
+		if ($result['error_code'] && $result['error_code'])
+		{
+			$this->saveToLog('WT Amo CRM - JoomShopping: ' . print_r($result, true), 'ERROR');
+		}
+
+		$lead_id = $result[0]->id;
+//		$contact_id = $result[0]->contact_id;
+
+		$notes = [];
+
+		if (!empty($order->order_add_info) && $this->params->get('amocrm_note_add_order_add_info', 1))
+		{
+			/**
+			 * note_type - 'common' - обычное текстовое.
+			 *           - 'extended_service_message' - Расширенное системное сообщение (поддерживает больше текста и сворачивается в интерфейсе). Если используется extended_service_message, то в params должен быть ключ 'service' = название сервиса от имени которого сообщение
+			 */
+			$notes[] = [
+				'created_by' => 0, // 0 - создал робот
+				'note_type'  => 'common',
+				'params'     => [
+					'text' => Text::_('PLG_WT_AMOCRM_JSHOPPING_AMOCRM_NOTE_ADD_ORDER_ADD_INFO_API') . $order->order_add_info,
+				]
+			];
+		}
+		// Сумма заказа в примечание
+		$notes[] = [
+			'created_by' => 0, // 0 - создал робот
+			'note_type'  => 'common',
+			'params'     => [
+				'text' => Text::_('PLG_WT_AMOCRM_JSHOPPING_AMOCRM_NOTE_ORDER_TOTAL_API_PREPEND') . \JSHelper::formatprice($order->order_total) . Text::_('PLG_WT_AMOCRM_JSHOPPING_AMOCRM_NOTE_ORDER_TOTAL_API_APPEND'),
+			]
+		];
+
+		if (count($orderItems) > 0 && $this->params->get('amocrm_note_order_items', 1) == 1)
+		{
+			$products_for_comment = '';
+			foreach ($orderItems as $item)
+			{
+				$products_for_comment .= Text::_('PLG_WT_AMOCRM_JSHOPPING_AMOCRM_NOTE_ORDER_ITEMS_PRODUCT') . $item->product_name . PHP_EOL;
+				$products_for_comment .= Text::_('PLG_WT_AMOCRM_JSHOPPING_AMOCRM_NOTE_ORDER_ITEMS_QUANTITY') . $item->product_quantity . PHP_EOL;
+				$products_for_comment .= Text::_('PLG_WT_AMOCRM_JSHOPPING_AMOCRM_NOTE_ORDER_ITEMS_PRICE') . $item->product_item_price . PHP_EOL;
+				if (!empty($item->product_attributes))
+				{
+					$products_for_comment .= Text::_('PLG_WT_AMOCRM_JSHOPPING_AMOCRM_NOTE_ORDER_ITEMS_PRODUCT_ATTRS') . $item->product_attributes . PHP_EOL;
+				}
+				if (!empty($item->weight))
+				{
+					$products_for_comment .= Text::_('PLG_WT_AMOCRM_JSHOPPING_AMOCRM_NOTE_ORDER_ITEMS_PRODUCT_WEIGHT') . $item->weight . PHP_EOL;
+				}
+
+				$products_for_comment .= PHP_EOL . ' ==== ' . PHP_EOL . PHP_EOL;
+
+			}
+
+			$notes[] = [
+				'created_by' => 0, // 0 - создал робот
+				'note_type'  => 'common',
+				'params'     => [
+					'text'    => Text::_('PLG_WT_AMOCRM_JSHOPPING_AMOCRM_NOTE_ORDER_ITEMS').PHP_EOL.$products_for_comment,
+				]
+			];
+		}
+		$this->prepareDebugInfo('Amo CRM NOTEs data before sending', [
+			'AmoCrm lead id' => $lead_id,
+			'notes'          => $notes
+		]);
+		$notes_result = $amocrm->addNotes('leads', $lead_id, $notes);
+		return true;
+	}
+
+
+	/**
+	 * Returns country name by id
+	 *
+	 * @param   int  $country_id
+	 *
+	 * @return string
+	 *
+	 * @since 1.0.0
+	 */
+	private function getCountryName($country_id) : string
+	{
+		$lang         = Factory::getApplication()->getLanguage();
+		$current_lang = $lang->getTag();
+		$db           = Factory::getContainer()->get('DatabaseDriver');
+		$query        = $db->getQuery(true);
+		$query->select($db->quoteName('name_' . $current_lang))
+			->from($db->quoteName('#__jshopping_countries'))
+			->where($db->quoteName('country_id') . ' = ' . $db->quote($country_id));
+		$db->setQuery($query);
+		$country_name = $db->loadAssoc();
+
+		return (string) $country_name["name_" . $current_lang];
+	}
+
+
+	/**
+	 * Returns coupon code by id
+	 *
+	 * @param   int  $coupon_id
+	 *
+	 * @return string
+	 *
+	 * @since 1.0.0
+	 */
+	private function getCouponCode($coupon_id) : string
+	{
+		$db    = Factory::getContainer()->get('DatabaseDriver');
+		$query = $db->getQuery(true);
+		$query->select($db->quoteName('coupon_code'))
+			->from($db->quoteName('#__jshopping_coupons'))
+			->where($db->quoteName('coupon_id') . ' = ' . $db->quote($coupon_id));
+		$db->setQuery($query);
+		$coupon_code = $db->loadAssoc();
+
+		return (string) $coupon_code["coupon_code"];
+	}
+
+
+	/**
+	 * Function checks the utm marks and set its to array fields
+	 *
+	 * @param   array  $lead_data  AmoCRM array data
+	 *
+	 * @return  array  AmoCRM array data with UTMs
+	 * @since    1.0.0
+	 */
+	private function checkUtms(&$lead_data): array
+	{
+		$utms = array(
+			'utm_source',
+			'utm_medium',
+			'utm_campaign',
+			'utm_content',
+			'utm_term',
+			'fbclid',
+			'yclid',
+			'gclid',
+			'gclientid',
+			'from',
+			'openstat_source',
+			'openstat_ad',
+			'openstat_campaign',
+			'openstat_service',
+			'referrer',
+			'roistat',
+			'_ym_counter',
+			'_ym_uid',
+			'utm_referrer'
+		);
+		foreach ($utms as $key)
+		{
+			$utm      = Factory::getApplication()->getInput()->cookie->get($key, '', 'raw');
+			$utm      = urldecode($utm);
+			$utm_name = strtoupper($key);
+			if (!empty($utm))
+			{
+				$utm_array                           = [
+					'field_code' => strtoupper($utm_name),
+					'values'     => [
+						[
+							'value' => $utm
+						]
+					]
+				];
+				$lead_data["custom_fields_values"][] = $utm_array;
+			}
+
+		}
+
+		return $lead_data;
+	}
+
+
+	/**
+	 * Добавляем js-скрпиты на HTML-фронт
+	 *
+	 * @throws \Exception
+	 * @since 1.0.0
+	 * @see   https://habr.com/ru/post/677262/
+	 */
+	function onAfterDispatch()
+	{
+		if($this->params->get('use_utm_js_script', 1) == 1){
+			// We are not work in Joomla API or CLI ar Admin area
+			if (!Factory::getApplication()->isClient('site')) return;
+
+			$doc = Factory::getApplication()->getDocument();
+			// We are work only in HTML, not JSON, RSS etc.
+			if (!($doc instanceof \Joomla\CMS\Document\HtmlDocument))
+			{
+				return;
+			}
+
+			$wa = $doc->getWebAssetManager();
+			// Show plugin version in browser console from js-script for UTM
+			$wt_amocrm_jshopping_plugin_info = simplexml_load_file(JPATH_SITE . "/plugins/system/wt_amocrm_jshopping/wt_amocrm_jshopping.xml");
+			$doc->addScriptOptions('plg_system_wt_amocrm_jshopping_version', $wt_amocrm_jshopping_plugin_info->version);
+			$wa->registerAndUseScript('plg_system_wt_amocrm_jshopping.wt_amocrm_jshopping_utm', 'plg_system_wt_amocrm_jshopping/wt_amocrm_jshopping_utm.js', array('version' => 'auto', 'relative' => true));
+		}
+
+	}
+
+	/**
+	 * Отправляем сделку в Амо СРМ только при успешной оплате.
+	 *
+	 * @param $text
+	 * @param $order_id
+	 *
+	 *
+	 * @throws \Exception
+	 * @since 1.0.0
+	 */
+	public function onBeforeDisplayCheckoutFinish(&$text, $order_id)
+	{
+
+		if ($this->params->get('order_trigger_event', 'always') == 'successful_payment')
+		{
+			$this->sendOrderToAmocrm($order_id);
+		}
+
+		if ($this->params->get('debug') == 1)
+		{
+			$session    = (Factory::getApplication() instanceof SessionAwareWebApplicationInterface ? Factory::getApplication()->getSession() : null);
+			$debug_info = $session->get("amocrmjshoppingdebugoutput");
+			echo "<h3>WT Amo CRM - JoomShopping debug information</h3><br/>" . $debug_info;
+			$session->clear("amocrmjshoppingdebugoutput");
+
+		}
+	}
+
+
+	/**
+	 * Function for to log library errors in plg_system_Wt_amocrm_jshopping.log.php in
+	 * Joomla log path. Default Log category plg_system_Wt_amocrm_jshopping
+	 *
+	 * @param   string  $data      error message
+	 * @param   string  $priority  Joomla Log priority
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 */
+	public function saveToLog(string $data, string $priority = 'NOTICE'): void
+	{
+		Log::addLogger(
+			array(
+				// Sets file name
+				'text_file' => 'plg_system_wt_amocrm_jshopping.log.php',
+			),
+			// Sets all but DEBUG log level messages to be sent to the file
+			Log::ALL & ~Log::DEBUG,
+			array('plg_system_wt_amocrm_jshopping')
+		);
+		Factory::getApplication()->enqueueMessage($data, $priority);
+		$priority = 'Log::' . $priority;
+		Log::add($data, $priority, 'plg_system_wt_amocrm_jshopping');
+
+	}
+
+}//plgSystemWt_amocrm_jshopping
